75633b42a9d0d2ae73add93486eda4ef8cfa85d8
swashvirus <johnswana49@gmail.com>
2022-06-21 23:55:53 +0000

    rebase-formatation--first-commit

--- sprite/src/index.ts
+++ sprite/src/index.ts
@@ -1,24 +1,24 @@
 import CRATERS from "craters"
 (async function main() {
-  const assetsManager = new CRATERS.AssetsManager()
-  await assetsManager.loadImage("./images/spritesheet.png")
-  .then(function (spritesheet) {
-    const canvas2DRenderer = new CRATERS.WebGLRenderer(500, 500)
-    const frames = [
+    const assetsManager = new CRATERS.AssetsManager()
+    await assetsManager.loadImage("./images/spritesheet.png")
+        .then(function(spritesheet) {
+            const canvas2DRenderer = new CRATERS.WebGLRenderer(500, 500)
+            const frames = [
                 [0, 0],
                 [0, 1],
                 [0, 2],
                 [1, 0],
                 [1, 1],
             ]
-    const sprite = new CRATERS.Sprite(canvas2DRenderer, spritesheet, 214, 282, frames, 1 / 25, 107, 140)
-    const renderLoop = new CRATERS.RenderLoop(function (renderLoop: any) {
-      canvas2DRenderer.clear()
-      sprite.draw(100, 100, renderLoop)
-    })
-    document.body.append(canvas2DRenderer.canvasElement)
-  })
-  .catch(function (err) {
-    throw err
-  })
+            const sprite = new CRATERS.Sprite(canvas2DRenderer, spritesheet, 214, 282, frames, 1 / 25, 107, 140)
+            const renderLoop = new CRATERS.RenderLoop(function(renderLoop: any) {
+                canvas2DRenderer.clear()
+                sprite.draw(100, 100, renderLoop)
+            })
+            document.body.append(canvas2DRenderer.canvasElement)
+        })
+        .catch(function(err) {
+            throw err
+        })
 })()
